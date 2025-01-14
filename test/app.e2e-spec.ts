c698c14e334ec14a9d2a824c84f0f2e3a764289c
Joao Tentis <101594270+jtentis@users.noreply.github.com>
2025-01-14 20:10:43 -0300

    Doc, feat: setando arquivos para execução de testes

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,12 +1,12 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('App E2E', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,10 +15,26 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should create a new product', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/products')
+      .send({
+        name: 'Product Test',
+        description: 'Description of the product',
+      });
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty('id');
+    expect(response.body.name).toBe('Product Test');
+  });
+
+  it('should retrieve all products', async () => {
+    const response = await request(app.getHttpServer()).get('/products');
+    expect(response.status).toBe(200);
+    expect(response.body).toBeInstanceOf(Array);
   });
 });
