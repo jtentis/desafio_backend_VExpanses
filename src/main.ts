6f0982df84ec2c578712cbd71bd71620c33f7827
Joao Tentis <101594270+jtentis@users.noreply.github.com>
2025-01-14 00:09:54 -0300

    Style: apenas trazendo o endpoint de autenticação para o topo do swagger

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Desafio Técnico Backend VExpanses')
     .setDescription('API para gerenciar planos de assinatura.')
+    .addTag('auth')
     .addBearerAuth()
     .setVersion('1.0')
     .build();
