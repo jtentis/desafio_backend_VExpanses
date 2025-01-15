d32f0bde9502f278ab573f6e5a588d6f11b44aba
Joao Tentis <101594270+jtentis@users.noreply.github.com>
2025-01-14 21:29:47 -0300

    Feat, doc: adicioando middleware global para pegar logs de todas as reqs http

---
+++ src/middleware/logging.middleware.ts
@@ -0,0 +1,24 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class LoggingMiddleware implements NestMiddleware {
+  private readonly logger = new Logger('HTTP');
+
+  use(req: Request, res: Response, next: NextFunction) {
+    const { method, originalUrl } = req;
+    const userAgent = req.get('user-agent') || '';
+    const ip = req.ip;
+
+    res.on('finish', () => {
+      const { statusCode } = res;
+      const contentLength = res.get('content-length') || 0;
+
+      this.logger.log(
+        `${method} ${originalUrl} ${statusCode} - ${contentLength}b sent - ${userAgent} ${ip}`,
+      );
+    });
+
+    next();
+  }
+}
