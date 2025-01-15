6323ec1911affd07bfa2515544d5a14436854953
Joao Tentis <101594270+jtentis@users.noreply.github.com>
2025-01-14 21:28:55 -0300

    Fix: ajeitando plans.service para atualizar historico de produtos quando um plano é criado

--- src/plans/plans.service.ts
+++ src/plans/plans.service.ts
@@ -4,87 +4,97 @@ import { CreatePlanDto } from './dto/create-plan.dto';
 
 @Injectable()
 export class PlansService {
-  constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService) { }
 
-  async create(createPlanDto: CreatePlanDto) {
-    const plan = await this.prisma.plan.create({
-      data: {
-        name: createPlanDto.name,
-        description: createPlanDto.description,
-        products: {
-          connect: createPlanDto.productIds.map(id => ({ id })),
-        },
-      },
-    });
-    return plan;
-  }
+    async create(createPlanDto: CreatePlanDto) {
+        const plan = await this.prisma.plan.create({
+            data: {
+                name: createPlanDto.name,
+                description: createPlanDto.description,
+                products: {
+                    connect: createPlanDto.productIds.map(id => ({ id })),
+                },
+            },
+        });
 
-  async addProductToPlan(planId: number, productId: number) {
-    const plan = await this.prisma.plan.update({
-      where: {
-        id: planId,
-      },
-      data: {
-        products: {
-          connect: {
-            id: productId,
-          },
-        },
-      },
-      include:{
-        products: true,
-      }
-    });
+        for (const productId of createPlanDto.productIds) {
+            await this.prisma.planHistory.create({
+                data: {
+                    action: `Produto ${productId} foi adicionado ao plano ${plan.name}.`,
+                    planId: plan.id,
+                    productId: productId,
+                },
+            });
+        }
+        return plan;
+    }
 
-    await this.prisma.planHistory.create({
-        data: {
-          action: `Produto ${productId} adcionado com sucesso!`,
-          planId: planId,
-          productId: productId,
-        },
-      });
+    async addProductToPlan(planId: number, productId: number) {
+        const plan = await this.prisma.plan.update({
+            where: {
+                id: planId,
+            },
+            data: {
+                products: {
+                    connect: {
+                        id: productId,
+                    },
+                },
+            },
+            include: {
+                products: true,
+            }
+        });
 
-    return plan;
-  }
+        await this.prisma.planHistory.create({
+            data: {
+                action: `Produto ${productId} adcionado com sucesso!`,
+                planId: planId,
+                productId: productId,
+            },
+        });
 
-  async removeProductFromPlan(planId: number, productId: number) {
-    const plan = await this.prisma.plan.update({
-      where: { id: planId },
-      data: {
-        products: {
-          disconnect: { id: productId },
-        },
-      },
-    });
+        return plan;
+    }
 
-    await this.prisma.planHistory.create({
-        data: {
-          action: `Produto ${productId} removido com sucesso!`,
-          planId: planId,
-          productId: productId,
-        },
-      });
-    return plan;
-  }
+    async removeProductFromPlan(planId: number, productId: number) {
+        const plan = await this.prisma.plan.update({
+            where: { id: planId },
+            data: {
+                products: {
+                    disconnect: { id: productId },
+                },
+            },
+        });
 
-  async getPlanDetails(planId: number) {
-    const plan = await this.prisma.plan.findUnique({
-      where: { id: planId },
-      include: {
-        products: true,
-        planHistory: true,
-      },
-    });
-    return plan;
-  }
+        await this.prisma.planHistory.create({
+            data: {
+                action: `Produto ${productId} removido com sucesso!`,
+                planId: planId,
+                productId: productId,
+            },
+        });
+        return plan;
+    }
 
-  async getAllPlanDetails() {
-    const plans = await this.prisma.plan.findMany({
-        include: {
-            products: true,
-            planHistory: true,
-        },
-    });
-    return plans;
-  }
+    async getPlanDetails(planId: number) {
+        const plan = await this.prisma.plan.findUnique({
+            where: { id: planId },
+            include: {
+                products: true,
+                planHistory: true,
+            },
+        });
+        return plan;
+    }
+
+    async getAllPlanDetails() {
+        const plans = await this.prisma.plan.findMany({
+            include: {
+                products: true,
+                planHistory: true,
+            },
+        });
+        return plans;
+    }
 }
